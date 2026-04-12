cc51b60b2c6d91466f41530bf71b6b371a112a56
nihardabhi <dabhi.nihar.5656@gmail.com>
2026-04-12 14:27:09 -0400

    add some changes to flight record test file

--- src/test/scala/edu/neu/csye7200/FlightRecordSpec.scala
+++ src/test/scala/edu/neu/csye7200/FlightRecordSpec.scala
@@ -5,37 +5,40 @@ import org.scalatest.matchers.should.Matchers
 
 class FlightRecordSpec extends AnyFlatSpec with Matchers {
 
-  // Valid CSV row
+  // BTS format: MONTH,DAY_OF_WEEK,FL_DATE,OP_UNIQUE_CARRIER,OP_CARRIER_FL_NUM,
+  // ORIGIN,ORIGIN_CITY_NAME,ORIGIN_STATE_ABR,DEST,DEST_CITY_NAME,DEST_STATE_ABR,
+  // CRS_DEP_TIME,DEP_TIME,DEP_DELAY,DEP_DEL15,CRS_ARR_TIME,ARR_TIME,ARR_DELAY,
+  // ARR_DEL15,CANCELLED,CANCELLATION_CODE,DISTANCE,CARRIER_DELAY,WEATHER_DELAY,
+  // NAS_DELAY,SECURITY_DELAY,LATE_AIRCRAFT_DELAY
+
   val validRow: Array[String] = Array(
-    "20241201", "12", "7", "AA", "1234",
+    "12", "1", "12/2/2024 12:00:00 AM", "AA", "1234",
     "JFK", "New York, NY", "NY",
     "LAX", "Los Angeles, CA", "CA",
-    "0800", "0815", "15.0", "1",
-    "1100", "1125", "25.0", "1",
-    "0", "", "2475.0",
-    "0.0", "25.0", "0.0", "0.0", "0.0"
+    "0800", "0815", "15.00", "1.00",
+    "1100", "1125", "25.00", "1.00",
+    "0.00", "", "2475.00",
+    "0.00", "25.00", "0.00", "0.00", "0.00"
   )
 
-  // Cancelled flight row
   val cancelledRow: Array[String] = Array(
-    "20241202", "12", "1", "DL", "5678",
+    "12", "1", "12/2/2024 12:00:00 AM", "DL", "5678",
     "ORD", "Chicago, IL", "IL",
     "ATL", "Atlanta, GA", "GA",
     "0900", "", "", "",
     "1200", "", "", "",
-    "1", "B", "600.0",
+    "1.00", "B", "600.00",
     "", "", "", "", ""
   )
 
-  // Severe delay row
   val severeRow: Array[String] = Array(
-    "20241203", "12", "3", "UA", "9999",
+    "12", "3", "12/3/2024 12:00:00 AM", "UA", "9999",
     "BOS", "Boston, MA", "MA",
     "SFO", "San Francisco, CA", "CA",
-    "0700", "0710", "10.0", "0",
-    "1000", "1250", "170.0", "1",
-    "0", "", "2704.0",
-    "0.0", "170.0", "0.0", "0.0", "0.0"
+    "0700", "0710", "10.00", "0.00",
+    "1000", "1250", "170.00", "1.00",
+    "0.00", "", "2704.00",
+    "0.00", "170.00", "0.00", "0.00", "0.00"
   )
 
   // --- Parsing tests ---
@@ -88,7 +91,15 @@ class FlightRecordSpec extends AnyFlatSpec with Matchers {
     FlightRecord.fromCSV(validRow).flatMap(_.weatherDelay) shouldBe Some(25.0)
   }
 
-  // --- delayCategory tests ---
+  it should "parse month correctly" in {
+    FlightRecord.fromCSV(validRow).map(_.month) shouldBe Some(12)
+  }
+
+  it should "parse dayOfWeek correctly" in {
+    FlightRecord.fromCSV(validRow).map(_.dayOfWeek) shouldBe Some(1)
+  }
+
+  // --- delayCategory boundary tests ---
 
   "delayCategory" should "return OnTime for negative delay" in {
     FlightRecord.fromCSV(validRow).get.copy(arrDelay = Some(-5.0)).delayCategory shouldBe OnTime
@@ -98,8 +109,20 @@ class FlightRecordSpec extends AnyFlatSpec with Matchers {
     FlightRecord.fromCSV(validRow).get.copy(arrDelay = Some(0.0)).delayCategory shouldBe OnTime
   }
 
+  it should "return Minor for 1 min delay" in {
+    FlightRecord.fromCSV(validRow).get.copy(arrDelay = Some(1.0)).delayCategory shouldBe Minor
+  }
+
   it should "return Minor for 25 min delay" in {
     FlightRecord.fromCSV(validRow).get.copy(arrDelay = Some(25.0)).delayCategory shouldBe Minor
+  }
+
+  it should "return Minor for exactly 29 min delay" in {
+    FlightRecord.fromCSV(validRow).get.copy(arrDelay = Some(29.0)).delayCategory shouldBe Minor
+  }
+
+  it should "return Major for exactly 30 min delay" in {
+    FlightRecord.fromCSV(validRow).get.copy(arrDelay = Some(30.0)).delayCategory shouldBe Major
   }
 
   it should "return Major for 45 min delay" in {
@@ -108,6 +131,10 @@ class FlightRecordSpec extends AnyFlatSpec with Matchers {
 
   it should "return Major for exactly 120 min delay" in {
     FlightRecord.fromCSV(validRow).get.copy(arrDelay = Some(120.0)).delayCategory shouldBe Major
+  }
+
+  it should "return Severe for exactly 121 min delay" in {
+    FlightRecord.fromCSV(validRow).get.copy(arrDelay = Some(121.0)).delayCategory shouldBe Severe
   }
 
   it should "return Severe for 170 min delay" in {
@@ -121,7 +148,8 @@ class FlightRecordSpec extends AnyFlatSpec with Matchers {
   // --- isDelayed tests ---
 
   "isDelayed" should "return true when arrDel15 = 1" in {
-    FlightRecord.fromCSV(validRow).map(_.isDelayed) shouldBe Some(true)
+    val r = FlightRecord.fromCSV(validRow).get.copy(arrDel15 = Some(1))
+    r.isDelayed shouldBe true
   }
 
   it should "return false when arrDel15 = 0" in {
